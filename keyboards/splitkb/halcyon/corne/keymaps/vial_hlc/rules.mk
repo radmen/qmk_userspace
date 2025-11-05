eef12085793c1c82b1f8ad2614f6163b51a2b8d1
Radoslaw Mejer <radmen@radmen.info>
2025-11-05 21:10:56 +0100

    Enable KEY_LOCK

--- keyboards/splitkb/halcyon/corne/keymaps/vial_hlc/rules.mk
+++ keyboards/splitkb/halcyon/corne/keymaps/vial_hlc/rules.mk
@@ -3,6 +3,7 @@ VIAL_ENABLE = yes
 VIALRGB_ENABLE = yes
 
 CAPS_WORD_ENABLE = yes
+KEY_LOCK_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
 
 # This adds module functionality to your keyboard (files found in users/halcyon_modules)
